b4d07649e70b6e87119b569222d6859e25f0941c
mihirbpi <mihir.borkar@gmail.com>
2023-03-08 11:58:26 -0800

    Fixed load-data

--- setup.sql
+++ setup.sql
@@ -1,75 +1,78 @@
-DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS responses;
 DROP TABLE IF EXISTS positions;
 DROP TABLE IF EXISTS contestants;
-DROP TABLE IF EXISTS responses;
-DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS games;
 
 -- Stores game information, uniquely represented by their game_id.
 CREATE TABLE games (
-    -- game ID, to be exactly 4 characters
-    game_id         CHAR(4) NOT NULL,
+    -- game ID
+    game_id         INT,
     -- season of the game, between 16 and 33
-    season          INT NOT NULL,
+    season          INT,
     PRIMARY KEY (game_id)
 );
 
 -- Stores contestant information, uniquely represented 
 -- by their player_id.
 CREATE TABLE contestants (
-    -- player ID, to be exactly 5 characters
-    player_id       CHAR(5),
+    -- player ID
+    player_id       INT,
     -- first name of the contestant
     first_name      VARCHAR(50) NOT NULL,
     -- last name of the contestant
     last_name       VARCHAR(50) NOT NULL,
     -- hometown city of the contestant
-    hometown_city   VARCHAR(100) NOT NULL,
+    hometown_city   VARCHAR(100),
     -- hometown state of the contestant
-    hometown_state  VARCHAR(100) NOT NULL,
+    hometown_state  VARCHAR(100),
     -- occupation of the contestant
-    occupation      VARCHAR(200) NOT NULL,
+    occupation      VARCHAR(200),
     PRIMARY KEY (player_id)
 );
 
 -- Stores podium information for each player in a game.
 CREATE TABLE positions (
-    -- game ID, to be exactly 4 characters
-    game_id         CHAR(4),
-    -- player ID, to be exactly 5 characters
-    player_id       CHAR(5) NOT NULL,
-    -- where player is standing (right, middle, returning champ)
+    -- game ID, to be up to 4 characters
+    game_id         INT,
+    -- where player is standing (right, middle, returning_champ)
+    player_id       INT,
     seat_location   VARCHAR(20),
-    PRIMARY KEY (game_id, seat_location)
+    -- player ID, to be up to 5 characters
+    PRIMARY KEY (game_id, seat_location),
+    FOREIGN KEY (game_id) REFERENCES games (game_id)
 );
 
 -- Stores response information (who answered questions),
 -- uniquely represented by the game_id and its location
 -- on the game board for each round.
 CREATE TABLE responses (
-    -- game ID, to be exactly 4 characters
-    game_id             CHAR(4),
+    -- game ID, to be up to 4 characters
+    game_id             INT,
     -- round number of the question
-    round               CHAR(5),
+    round               VARCHAR(2),
     -- row index of question on the board
-    row_idx             INT NOT NULL,
+    row_idx             INT,
     -- column index of question on the board
-    column_idx          INT NOT NULL,
+    column_idx          INT,
     -- position of the contestant who answered the question, 
-    correct_respondent  VARCHAR(15),
+    correct_respondent  VARCHAR(20),
     -- position of the contestant who chose the question
-    asker               VARCHAR(15) NOT NULL,
-    -- amount contestant wagered on the question, if Double Jeopardy
-    wager               VARCHAR(6),
-    PRIMARY KEY (game_id, round, row_idx, column_idx)
+    asker               VARCHAR(20) NOT NULL,
+    -- amount contestant wagered on the question, if Daily Double
+    wager               INT,
+    PRIMARY KEY (game_id, round, row_idx, column_idx),
+    FOREIGN KEY (game_id, asker) 
+        REFERENCES positions (game_id, seat_location)
 );
 
 -- Stores questions, uniquely represented by their game_id
 -- and its location on the game board for each round.
 CREATE TABLE questions (
     -- game ID, to be exactly 4 characters
-    game_id             CHAR(4),
+    game_id             INT,
     -- round of the question
-    round               CHAR(5),
+    round               VARCHAR(2),
     -- row index of question on the board
     row_idx             INT,
     -- column index of question on the board
@@ -77,7 +80,7 @@ CREATE TABLE questions (
     -- category of the question
     category            VARCHAR(500) NOT NULL,
     -- value of the question, in dollars
-    value               INT NOT NULL,
+    question_value      INT NOT NULL,
     -- the question itself
     question_text       VARCHAR(1000),
     -- the answer to the question
