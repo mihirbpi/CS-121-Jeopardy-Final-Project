375c9f8b48d42c0f6948be6286e0959eaed2a8db
mihirbpi <mihir.borkar@gmail.com>
2023-03-11 14:08:10 -0800

    Change questions csv

--- setup.sql
+++ setup.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS value_mapping;
 DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS responses;
 DROP TABLE IF EXISTS positions;
@@ -84,18 +85,6 @@ CREATE TABLE responses (
     CHECK (correct_respondent IN ('right', 'middle', 'returning_champ', NULL))
 );
 
--- Stores value of question based on its location on the board and the round.
-CREATE TABLE value_mapping (
-    -- round of the question
-    round               VARCHAR(5),
-    -- row index of question on the board
-    row_idx             TINYINT,
-    -- value of the question, in dollars (100-2000, -1 if final jeopardy)
-    question_value      VARCHAR(4) NOT NULL,
-    PRIMARY KEY (round, row_idx),
-    CHECK (round IN ('J', 'DJ', 'final'))
-);
-
 -- Stores questions, uniquely represented by their game_id
 -- and its location on the game board for each round.
 CREATE TABLE questions (
@@ -114,6 +103,18 @@ CREATE TABLE questions (
     -- the answer to the question
     answer              TEXT,
     PRIMARY KEY (game_id, round, row_idx, column_idx),
+    CHECK (round IN ('J', 'DJ', 'final'))
+);
+
+-- Stores value of question based on its location on the board and the round.
+CREATE TABLE value_mapping (
+    -- round of the question
+    round               VARCHAR(5),
+    -- row index of question on the board
+    row_idx             TINYINT,
+    -- value of the question, in dollars (100-2000, -1 if final jeopardy)
+    question_value      VARCHAR(4) NOT NULL,
+    PRIMARY KEY (round, row_idx),
     CHECK (round IN ('J', 'DJ', 'final'))
 );
 
