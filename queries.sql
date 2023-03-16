91062b3d3fe86665ae560d0755ddd5a0177d2761
mihirbpi <mihir.borkar@gmail.com>
2023-03-16 10:01:43 -0700

    issues with queries

--- queries.sql
+++ queries.sql
@@ -46,12 +46,12 @@ WITH cp AS (SELECT * FROM positions NATURAL LEFT JOIN contestants NATURAL LEFT J
                    GROUP BY cp.first_name, cp.last_name)
 SELECT season, AVG(avg_score * 1.0) as season_avg_score
 FROM (
-        SELECT CONCAT(c.first_name, ' ', c.last_name) AS contestant, ng.season as season,
+        SELECT CONCAT(c.first_name, ' ', c.last_name) AS contestant, ng.season AS season,
             SUM(question_points(j.chooser, j.correct_respondent, j.question_value, j.wager)) / ng.num AS avg_score
-        FROM (SELECT * FROM games NATURAL LEFT JOIN responses NATURAL LEFT JOIN value_mapping) j
+        FROM (SELECT * FROM games NATURAL LEFT JOIN responses NATURAL LEFT JOIN value_mapping) AS j
         INNER JOIN positions p ON j.correct_respondent = p.seat_location AND j.chooser = p.seat_location AND j.game_id = p.game_id
-        INNER JOIN contestants c ON p.player_id = c.player_id
-        INNER JOIN num_games ng ON CONCAT(c.first_name, ' ', c.last_name) = ng.contestant
+        INNER JOIN contestants AS c ON p.player_id = c.player_id
+        INNER JOIN num_games AS ng ON CONCAT(c.first_name, ' ', c.last_name) = ng.contestant
         GROUP BY j.chooser, c.first_name, c.last_name, ng.season
      ) AS t
 GROUP BY season
